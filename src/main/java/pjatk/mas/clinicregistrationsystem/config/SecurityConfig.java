5ac26fbb2b5a841b1d4c566226a85e3e667b92df
Patrycja Szpakowska <patrycja.szpakowska@gmail.com>
2026-05-25 11:53:58 +0200

    Add home page to permit all

--- src/main/java/pjatk/mas/clinicregistrationsystem/config/SecurityConfig.java
+++ src/main/java/pjatk/mas/clinicregistrationsystem/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "/css/**", "/js/**", "/images/**").permitAll()
+                        .requestMatchers("/", "/login", "/css/**", "/js/**", "/images/**").permitAll()
                         .requestMatchers("/h2-console/**").permitAll()
                         .requestMatchers("/my-appointments/**", "/my-profile/**").hasRole("PATIENT")
                         .requestMatchers("/employees/**", "/payroll/**").hasRole("MANAGER")
