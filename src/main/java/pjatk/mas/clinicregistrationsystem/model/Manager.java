941de023776648d2d278d78af35af0d3caed3027
Patrycja Szpakowska <patrycja.szpakowska@gmail.com>
2026-05-28 16:03:03 +0200

    Add factory methods to manager

--- src/main/java/pjatk/mas/clinicregistrationsystem/model/Manager.java
+++ src/main/java/pjatk/mas/clinicregistrationsystem/model/Manager.java
@@ -1,6 +1,7 @@
 package pjatk.mas.clinicregistrationsystem.model;
 
 import jakarta.persistence.*;
+import pjatk.mas.clinicregistrationsystem.model.enums.Shift;
 import pjatk.mas.clinicregistrationsystem.model.enums.SeniorityLevel;
 
 import java.time.LocalDate;
@@ -16,15 +17,38 @@ public class Manager extends Employee {
 
     protected Manager() {}
 
-    public Manager(String firstName, String lastName, LocalDate dateOfBirth,
+    private Manager(String firstName, String lastName, LocalDate dateOfBirth,
                    Address address, String phoneNumber, String emailAddress,
                    String jobTitle, float hourlyRate, String bankAccount,
                    String login, String password, String documentId,
+                   ContractType contractType,
                    SeniorityLevel seniorityLevel) {
         super(firstName, lastName, dateOfBirth, address, phoneNumber, emailAddress,
-                jobTitle, hourlyRate, bankAccount, login, password, documentId);
+                jobTitle, hourlyRate, bankAccount, login, password, documentId, contractType);
         if (seniorityLevel == null) throw new IllegalArgumentException("Seniority level cannot be null");
         this.seniorityLevel = seniorityLevel;
+    }
+
+    public static Manager createWithFullTime(String firstName, String lastName, LocalDate dateOfBirth,
+                                             Address address, String phoneNumber, String emailAddress,
+                                             String jobTitle, float hourlyRate, String bankAccount,
+                                             String login, String password, String documentId,
+                                             SeniorityLevel seniorityLevel,
+                                             float overtimeRate) {
+        return new Manager(firstName, lastName, dateOfBirth, address, phoneNumber, emailAddress,
+                jobTitle, hourlyRate, bankAccount, login, password, documentId,
+                FullTime.create(overtimeRate), seniorityLevel);
+    }
+
+    public static Manager createWithPartTime(String firstName, String lastName, LocalDate dateOfBirth,
+                                             Address address, String phoneNumber, String emailAddress,
+                                             String jobTitle, float hourlyRate, String bankAccount,
+                                             String login, String password, String documentId,
+                                             SeniorityLevel seniorityLevel,
+                                             Shift shift, int hoursInContract) {
+        return new Manager(firstName, lastName, dateOfBirth, address, phoneNumber, emailAddress,
+                jobTitle, hourlyRate, bankAccount, login, password, documentId,
+                PartTime.create(shift, hoursInContract), seniorityLevel);
     }
 
     @Transient
